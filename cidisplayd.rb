f842322788a660320599a922d7452ef6d96ff437
pascalbetz <pascal.betz@simplificator.com>
2011-01-07 11:56:00 +0100

    added require statement

--- cidisplayd.rb
+++ cidisplayd.rb
@@ -1,7 +1,8 @@
+require 'daemons'
 require 'cidisplay'
 
 credentials = YAML.load_file(File.dirname(__FILE__) + '/hudson.yml')
-ci_display = CiDisplay.new(credentials).publish
+ci_display = CiDisplay.new(credentials)
 
 Daemons.run_proc('cidsiplay.rb') do
   ci_display.publish
