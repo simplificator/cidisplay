196748bae5c3745466e7f06f80f897cce76dc9a5
pascalbetz <pascal.betz@simplificator.com>
2011-01-07 11:54:16 +0100

    renamed files, added deamon

---
+++ cidisplay.rb
@@ -0,0 +1,74 @@
+require 'rubygems'
+require 'yaml'
+require 'hudson-remote-api'
+require 'rdis'
+require 'serialport'
+
+class CiDisplay
+  COLORS = {'red' => 1, 'red_anime' => 2, 'blue' => 3, 'blue_anime' => 4, 'grey' => 5}
+
+  def initialize(credentials, device = '/dev/tty.usbserial')
+    credentials.each do |key, value|
+      Hudson[key.to_sym] = value
+    end
+    @device = device
+  end
+
+
+  def publish
+    jobs = fetch_sorted_jobs
+    board = open_board(@device)
+    board.deliver build_message(jobs)
+  end
+
+  private
+  def fetch_sorted_jobs
+    jobs = Hudson::Job.list.map do |name|
+      Hudson::Job.new(name)
+    end.sort() do |a, b|
+      (COLORS[a.color] || 10) <=> (COLORS[b.color] || 10)
+    end
+  end
+
+  def open_board(device)
+    board = Rdis::Board.new(device)
+    board.open
+    board
+  end
+
+  def build_message(jobs)
+    message = Rdis::Message.new(:method => Rdis::DisplayMethodElement::LEVEL_1_NORMAL)
+    jobs.each_with_index do |job, index|
+      case job.color
+      when 'red'
+        message.add(Rdis::ColorElement::RED)
+        add_job_name(message, job, index)
+      when 'red_anime'
+        message.add(Rdis::ColorElement::DIM_RED)
+        add_job_name(message, job, index)
+      when 'blue'
+        message.add(Rdis::ColorElement::GREEN)
+        add_job_name(message, job, index)
+      when 'blue_anime'
+        message.add(Rdis::ColorElement::DIM_GREEN)
+        add_job_name(message, job, index)
+      when 'grey'
+        message.add(Rdis::ColorElement::YELLOW)
+        add_job_name(message, job, index)
+      else
+        message.add(Rdis::ColorElement::RAINBOW)
+        add_job_name(message, job, index)
+      end
+    end
+    message
+  end
+
+  def add_job_name(message, job, index)
+    if index > 0
+      message.add " | "
+    end
+    message.add(job.name)
+  end
+end
+
+
---
+++ cidisplayd.rb
@@ -0,0 +1,9 @@
+require 'cidisplay'
+
+credentials = YAML.load_file(File.dirname(__FILE__) + '/hudson.yml')
+ci_display = CiDisplay.new(credentials).publish
+
+Daemons.run_proc('cidsiplay.rb') do
+  ci_display.publish
+  sleep(120)
+end
