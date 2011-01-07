80c34c6dd03495ae38454dcd676cb2f2f21c7c22
pascalbetz <pascal.betz@simplificator.com>
2011-01-07 12:20:56 +0100

    added daemon options

--- cidisplayd.rb
+++ cidisplayd.rb
@@ -7,7 +7,7 @@ puts "Using Device #{device}"
 credentials = YAML.load_file(File.dirname(__FILE__) + '/hudson.yml')
 ci_display = CiDisplay.new(credentials, device)
 
-Daemons.run_proc('cidsiplay.rb') do
+Daemons.run_proc('cidsiplay.rb', :log_output => true, :multiple => false, :monitor => true) do
   ci_display.publish
   sleep(120)
 end
