1b218a3d4f4a10f12557dd1ea0ee99e5f86dd9c5
pascalbetz <pascal.betz@simplificator.com>
2011-01-07 12:16:49 +0100

    passing device for serialport

--- cidisplayd.rb
+++ cidisplayd.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 require 'daemons'
 require 'cidisplay'
 
+device = ARGV[1] || '/dev/tty.usbserial'
+puts "Using Device #{device}"
 credentials = YAML.load_file(File.dirname(__FILE__) + '/hudson.yml')
-ci_display = CiDisplay.new(credentials)
+ci_display = CiDisplay.new(credentials, device)
 
 Daemons.run_proc('cidsiplay.rb') do
   ci_display.publish
