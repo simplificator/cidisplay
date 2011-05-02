d400dd0d8bbfcf0f72e0253efb9c54b72be1ccff
pascalbetz <pascal.betz@simplificator.com>
2011-05-02 12:39:10 +0200

    fixed argument index for ARGV

--- runonce.rb
+++ runonce.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'cidisplay'
 
-device = ARGV[1] || '/dev/tty.usbserial'
+device = ARGV[0] || '/dev/tty.usbserial'
 puts "Using Device #{device}"
 credentials = YAML.load_file(File.dirname(__FILE__) + '/hudson.yml')
 ci_display = CiDisplay.new(credentials, device)
