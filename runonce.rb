3cb9be93c69db3f418022ebd46fa07a439266b7f
Pascal Betz <pascal.betz@simplificator.com>
2012-11-19 15:55:33 +0100

    enable both sources

--- runonce.rb
+++ runonce.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
-require 'cidisplay'
-require File.dirname(__FILE__) + '/hudons'
+require 'ci'
+require File.dirname(__FILE__) + '/hudson'
+
 require File.dirname(__FILE__) + '/semaphore'
+require File.dirname(__FILE__) + '/combined'
 
 
 
@@ -9,6 +11,6 @@ device = ARGV[0] || '/dev/tty.usbserial'
 puts "Using Device #{device}"
 
 combined = Combined.new(device)
-#combined << Hudson.new(YAML.load_file(File.dirname(__FILE__) + '/hudson.yml'), device)
+combined << Hudson.new(YAML.load_file(File.dirname(__FILE__) + '/hudson.yml'), device)
 combined << Semaphore.new("77Uce5itpFox641nxXxE", device)
 combined.publish
